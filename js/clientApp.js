b98c7ab86365d59ebb26bbec4d133915c8184ab2
Jsumulong <sumulong28@gmail.com>
2017-01-20 00:28:39 -0600

    add myTitle

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,11 +1,21 @@
 		var div = React.DOM.div
 		var h1 = React.DOM.h1
 
+		var myTitle = React.createClass({
+			render: function() {
+				return (
+					div(null, 
+						h1(null, 'check out this component')
+					)
+				) 
+			}
+		})
+
 		var myFirstComponent = React.createClass({
 			render: function() {
 				return (
 					div(null, 
-						h1(null, 'This is my first component!')
+						React.createElement(myTitle)
 					)
 				)
 			}
