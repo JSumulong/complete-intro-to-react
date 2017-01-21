1f7a317a4b7d9e20586b13725aacf06e1870d1d6
Jsumulong <sumulong28@gmail.com>
2017-01-20 23:18:25 -0600

    configure webpack, import react, react-dom, and myTitle module in clientApp.js and bundle with webpack, configure babel to translate react and ES6 to ES5

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,30 +1,23 @@
-		var div = React.DOM.div
-		var h1 = React.DOM.h1
+import React from 'react'
+import ReactDOM from 'react-dom'
+import myTitle from './myTitle'
 
-		var myTitle = React.createClass({
-			render: function() {
-				return (
-					div(null, 
-						h1({style: {color: this.props.color}}, this.props.title)
-					)
-				) 
-			}
-		})
+var div = React.DOM.div
 
-		var myTitleFactory = React.createFactory(myTitle)
+var myTitleFactory = React.createFactory(myTitle)
 
-		var myFirstComponent = React.createClass({
-			render: function() {
-				return (
-					div(null, 
+var myFirstComponent = React.createClass({
+  render: function () {
+    return (
+					div(null,
 						myTitleFactory({title: "It\'s the first line", color: 'peru'}),
 						myTitleFactory({title: 'This is a title prop', color: 'orange'}),
 						myTitleFactory({title: 'Oh, ok', color: 'papayawhip'}),
 						myTitleFactory({title: 'Here is another one', color: 'rebeccapurple'}),
 						myTitleFactory({title: 'and last, but not least', color: 'pink'})
 					)
-				)
-			}
-		})
+    )
+  }
+})
 
-		ReactDOM.render(React.createElement(myFirstComponent), document.getElementById('app'))
+ReactDOM.render(React.createElement(myFirstComponent), document.getElementById('app'))
